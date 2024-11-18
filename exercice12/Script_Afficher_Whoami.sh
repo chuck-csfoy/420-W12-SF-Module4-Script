96891c4f71119d50006830236b38a3f027eb3623
cpwarren <2497992@csfoy.ca>
2024-11-17 20:58:35 -0500

    exercice12 presque complete

--- exercice12/Script_Afficher_Whoami.sh
+++ exercice12/Script_Afficher_Whoami.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# declaration de la variable DATE avec initialisation au parametres demandes.
+# cmd date + les format control
+DATE=$(date +"%A %d %B à %H:%M:%S")
 # Script pour afficher un message de bienvenue avec le d'utilisateur et le nom de la machine
 
 echo "Bien dans le terminal BASH $(whoami)!"
 echo "Nous sommes heureux que votre machine $HOSTNAME se porte tres bien aussi!"
 
 # Afficher date et heure actuelle.
-echo "Date et heure actuelles: $(date)"
+echo "Date et heure actuelles: $DATE"
