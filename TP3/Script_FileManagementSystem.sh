fcb12a1ad79168004c74aebf79ff58d7a3da713f
cpwarren <2497992@csfoy.ca>
2024-11-24 16:32:15 -0500

    Script_FileManagementSystem.sh with directory creation done

--- TP3/Script_FileManagementSystem.sh
+++ TP3/Script_FileManagementSystem.sh
@@ -5,10 +5,10 @@
 username=""
 
 # Prompt user to ask a username input. read -p is used for this task and is similar to echo and read on two seperate lines.
-read -p "Enter the username to be created" username
+read -p "Enter the username to be created." username
 
 if [ -z "$username" ]; then
-  echo "Error : No username entry. Username cannot be empty"
+  echo "Error : No username entered. Username cannot be empty."
 fi
 
 # username creation user adduser.
@@ -19,6 +19,28 @@ fi
 if [[ id "$username" &>/dev/null ]]; then
   echo "Username '$username' was created with success."
   else
-   echo "Error : Username '$username' already exist."
+    echo "Error : Username '$username' already exist."
   fi
+fi
+
+# This portion of the script is used to ask user to choose a name for a new directory name.
+# directory name variable declaration.
+directory_name=""
+
+# Prompt user tp choose a directory name
+read -p "Enter the directory name to be created." directory_name
+
+# User input verification to check if entry is empty.
+if [ -z "$directory_name" ]; then
+  echo "Error : No directory name entered. Directory name cannot be empty."
+fi
+
+# Directory creation with option -p to check if the directory already exist. If so it won't be created.
+mkdir -p "$directory_name"
+
+# Directoy creation verification with option -eq (equal) that the operation was successful.
+if [ $? -eq 0 ]; then
+  echo "Directory '$directory_name' was created with success"
+else
+  echo "Error : An error occured during directory name creation"
 fi
