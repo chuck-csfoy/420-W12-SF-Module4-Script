4ddca749a44aa05c3c7c150838d476a53bf913fc
cpwarren <2497992@csfoy.ca>
2024-11-24 21:28:45 -0500

    TP3 Script Q2 debugged

--- TP3/Script_FileManagementSystem.sh
+++ TP3/Script_FileManagementSystem.sh
@@ -5,7 +5,7 @@
 username=""
 
 # Prompt user to ask a username input. read -p is used for this task and is similar to echo and read on two seperate lines.
-read -p "Enter the username to be created." username
+read -p "Enter the username to be created: " username
 
 if [ -z "$username" ]; then
   echo "Error : No username entered. Username cannot be empty."
@@ -13,14 +13,13 @@ fi
 
 # username creation user adduser.
   echo "username id creation '$username'..."
-  sudo adduser "$username"
+  sudo useradd "$username"
 
 # username verification if it does not exist already.
-if [[ id "$username" &>/dev/null ]]; then
-  echo "Username '$username' was created with success."
+if ! id "$username" &>/dev/null; then
+  echo "Error : Username '$username' already exist."
   else
-    echo "Error : Username '$username' already exist."
-  fi
+    echo "Username '$username' was created with success."
 fi
 
 # This portion of the script is used to ask user to choose a name for a new directory name.
@@ -28,7 +27,7 @@ fi
 directory_name=""
 
 # Prompt user tp choose a directory name
-read -p "Enter the directory name to be created." directory_name
+read -p "Enter the directory name to be created: " directory_name
 
 # User input verification to check if entry is empty.
 if [ -z "$directory_name" ]; then
@@ -42,15 +41,14 @@ mkdir -p "$directory_name"
 echo "Directory '$directory_name' was created with success"
 
 # User permissions changed using chmod as per requirements so that only the directory owner has access.
-# go is used to remove rights from group and other users.
-chmod u+rwx, go-rwx
+chmod 700 "$directory_name"
 
 # Script to as user to select a new filename and to generate the new file if it does not exist already.
 # varibale declaration for filename.
 file_name=""
 
 # Prompt user to choose a file name.
-read -p "Enter a file name to be created :" file_name
+read -p "Enter a file name to be created: " file_name
 
 # User input verifiaction if entry is not empty.
 if [ -z "file_name" ]; then
@@ -59,7 +57,7 @@ fi
 
 # File creation with option -c to check if the file already exist. If so it won't be created.
 touch -c "$file_name"
-# Similar to with mkdir -p above. Verification with option -c is shorter but equivalent to use: if [ $? -eq 0 ]; then...
+# Similar to with mkdir p above. Verification with option c.
 echo "File '$file_name' was created with success."
 
 # Prompt user with welcome message.
@@ -70,7 +68,7 @@ echo "We are please to see that your machine $HOSTNAME is also doing well"
 # awk cmd is used to extract and print the first column form the ls -ld output which is the persmissions of the directory.
 # {prnt $1} is use to tell awk to print the first (1st) field wich is (drwxr-xr-x).
 # Redirectin operator is used to add (append) the output of the cmd to a file.
-echo "Your work directory is '$directory', which has the following permissions: $(ls -ld "directory_name" | awk '{print $1}')." >> "file_name"
+echo "Your work directory is '$directory_name' has the following permissions: $(ls -ld "$directory_name" | awk '{print $1}')." >> "file_name"
 # hostname -I is used to display ip address with the same commands to add info to file.
 echo "Your IP address is : $(hostname -I | awk '{print $1}')." >> "$file_name"
 
